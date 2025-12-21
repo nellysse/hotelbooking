732b8eb4f3096c1375c3e0f9a8563bee06ddbc04
nel <zhanel.usonkulova@alatoo.edu.kg>
2025-12-21 17:53:25 +0600

    Refactor Reservation class to simplify getters

--- HOTelbkn/src/main/java/org/example/hotelbkn/Reservation.java
+++ HOTelbkn/src/main/java/org/example/hotelbkn/Reservation.java
@@ -25,29 +25,16 @@ public class Reservation {
     public void setReservationId(int reservationId) { this.reservationId = reservationId; }
 
     public String getGuestName() { return guestName; }
-    public void setGuestName(String guestName) { this.guestName = guestName; }
-
     public String getRoomNumber() { return roomNumber; }
-    public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
-
     public String getContactNumber() { return contactNumber; }
-    public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
-
     public LocalDate getCheckInDate() { return checkInDate; }
-    public void setCheckInDate(LocalDate checkInDate) { this.checkInDate = checkInDate; }
-
     public LocalDate getCheckOutDate() { return checkOutDate; }
-    public void setCheckOutDate(LocalDate checkOutDate) { this.checkOutDate = checkOutDate; }
-
+    
     public String getPeriod() {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        if (checkInDate != null && checkOutDate != null) {
-            return fmt.format(checkInDate) + " - " + fmt.format(checkOutDate);
-        } else if (checkInDate != null) {
-            return fmt.format(checkInDate);
-        } else {
-            return "";
-        }
+        return fmt.format(checkInDate) + " - " + fmt.format(checkOutDate);
     }
 }
+
+
 
